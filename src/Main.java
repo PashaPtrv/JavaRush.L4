bbcd372a16fd70cd4505d03fb3de4c448355ffa2
Pasha <pvlptrv@yandex.ru>
2022-03-08 02:25:26 +0300

    Лекция 4, блок заданий 3.3

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    //Вывести на экран числа от 10 до 1, используя цикл while.
+    //Ввести с клавиатуры строку и число N.
+    //Вывести на экран строку N раз, используя цикл while.
 
     public static void main(String[] args) {
-        int num = 10;
-        while (num >= 1){
-            System.out.println(num);
-            num--;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String str = scanner.nextLine();
+        System.out.println("Введите количество выводов:");
+        int count = scanner.nextInt();
+        int num = 1;
+        while (num <= count){
+            System.out.println(str);
+            num++;
         }
 
     }
