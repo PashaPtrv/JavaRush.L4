11175a92b824ac999e8375cd91a2cca1758a57d7
Pasha <pvlptrv@yandex.ru>
2022-03-08 02:15:16 +0300

    Лекция 4, блок заданий 3.2

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    //Вывести на экран числа от 1 до 10, используя цикл while.
+    //Вывести на экран числа от 10 до 1, используя цикл while.
 
     public static void main(String[] args) {
-        int num = 1;
-        while (num <= 10){
+        int num = 10;
+        while (num >= 1){
             System.out.println(num);
-            num++;
+            num--;
         }
 
     }
