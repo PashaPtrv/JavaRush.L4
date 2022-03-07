0ce4cbdb03019a907a84276eab068a07183c7fa2
Pasha <pvlptrv@yandex.ru>
2022-03-08 02:00:09 +0300

    Лекция 4, блок заданий 2.6

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
 
     //Ввести с клавиатуры имя и возраст. Если возраст меньше 18 вывести надпись «Подрасти еще»
+    //Ввести с клавиатуры имя и возраст.
+    //Если возраст больше 20 вывести надпись «И 18-ти достаточно»
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите имя: ");
@@ -10,6 +12,7 @@ public class Main {
         System.out.println("Введите возраст: ");
         int age = scanner.nextInt();
         if (age < 18) System.out.println("Подрасти еще");
+        else if (age > 20) System.out.println("И 18-ти достаточно");
 
     }
 }
