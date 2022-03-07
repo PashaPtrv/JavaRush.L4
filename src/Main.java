bb022c52c5997018a490a347b3a9a23f6c58dc5c
Pasha <pvlptrv@yandex.ru>
2022-03-08 01:57:40 +0300

    Лекция 4, блок заданий 2.5

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    /* Ввести с клавиатуры два имени, и если имена одинаковые, вывести сообщение «Имена идентичны».
-    Если имена разные, но их длины равны – вывести сообщение – «Длины имен равны». */
+    //Ввести с клавиатуры имя и возраст. Если возраст меньше 18 вывести надпись «Подрасти еще»
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String name1 = scanner.nextLine();
-        String name2 = scanner.nextLine();
-        if (name1.equals(name2)) {
-            System.out.println("Имена идентичны");
-        } else if (name1.length() == name2.length()){
-            System.out.println("Длинны имен равны");
-        } else System.out.println("Имена разные");
+        System.out.println("Введите имя: ");
+        String name = scanner.nextLine();
+        System.out.println("Введите возраст: ");
+        int age = scanner.nextInt();
+        if (age < 18) System.out.println("Подрасти еще");
+
     }
 }
 
