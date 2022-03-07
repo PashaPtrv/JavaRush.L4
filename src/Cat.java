6ae5d04bc69671fe015f6f4f63cb22dc0b8c79d8
Pasha <pvlptrv@yandex.ru>
2022-03-07 22:37:33 +0300

    Лекция 4, блок заданий 1.1

--- src/Cat.java
+++ src/Cat.java
@@ -1,13 +1,20 @@
 public class Cat {
-    /*Реализовать метод setName, чтобы с его помощью можно было устанавливать значение
-     переменной private String name равное переданному параметру String name.*/
-    private String name;
-    public void setName(String name){
-        this.name = name;
+
+    /*Реализовать метод addNewCat,
+     чтобы при его вызове (т.е. добавлении нового кота), количество котов увеличивалось на 1.
+      За количество котов отвечает переменная catsCount.
+     */
+    private static int catsCount = 0;
+    public static void addNewCat() {
+        catsCount++;
     }
+
+
     public static void main(String[] args){
-        Cat cat = new Cat();
-        cat.setName("Бублик");
-        System.out.println(cat.name);
+        addNewCat();
+        addNewCat();
+        addNewCat();
+        System.out.println(catsCount);
+
     }
 }
