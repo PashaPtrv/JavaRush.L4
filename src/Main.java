53af19c9f44995dcc88a3ed87d9ed8de37b607d1
Pasha <pvlptrv@yandex.ru>
2022-03-08 02:13:33 +0300

    Лекция 4, блок заданий 3.1

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 
 public class Main {
 
-    //Ввести с клавиатуры имя и возраст. Если возраст меньше 18 вывести надпись «Подрасти еще»
-    //Ввести с клавиатуры имя и возраст.
-    //Если возраст больше 20 вывести надпись «И 18-ти достаточно»
+    //Вывести на экран числа от 1 до 10, используя цикл while.
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя: ");
-        String name = scanner.nextLine();
-        System.out.println("Введите возраст: ");
-        int age = scanner.nextInt();
-        if (age < 18) System.out.println("Подрасти еще");
-        else if (age > 20) System.out.println("И 18-ти достаточно");
+        int num = 1;
+        while (num <= 10){
+            System.out.println(num);
+            num++;
+        }
 
     }
 }
