2d86609bc889057ec43dec3f170d9e2eff484067
Pasha <pvlptrv@yandex.ru>
2022-03-08 01:12:38 +0300

    Лекция 4, блок заданий 2.2

--- src/Cat.java
+++ src/Cat.java
@@ -1,13 +1,4 @@
 public class Cat {
- /*Написать код, который бы подсчитывал количество созданных котов (count)
-  и на экран выдавалось правильно их количество.*/
- public static int count = 0;
-    public static void main(String[] args){
-        Cat cat1 = new Cat();
-       Cat.count++;
-        Cat cat2 = new Cat();
-        Cat.count++;
-        System.out.println("Cats count is " + Cat.count);
-    }
+
 }
 
--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    //Ввести с клавиатуры два числа, и вывести на экран минимальное из них.
-    public static void main(String[] args){
+    //Ввести с клавиатуры четыре числа, и вывести максимальное из них.
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
-        if (num1 > num2) {
+        int num3 = scanner.nextInt();
+        int num4 = scanner.nextInt();
+        if (num1 > num2 && num1 > num3 && num1 > num4){
+            System.out.println(num1);
+        } else if (num2 > num1 && num2 > num3 && num2 > num4){
             System.out.println(num2);
-        }
-         else if (num1 == num2){
-            System.out.println("Числа равны!");
-        } else System.out.println(num1);;
+        } else if (num3 > num1 && num3 > num2 && num3 > num4){
+            System.out.println(num3);
+        } else if (num4 > num1 && num4 > num2 && num4 > num3){
+            System.out.println(num4);
+        } else System.out.println("Все числа равны!");
     }
-
 }
+
+
 
