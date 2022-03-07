2c8d10e99da0c238a4bf95ff46202d7ffb1b7e23
Pasha <pvlptrv@yandex.ru>
2022-03-08 00:42:09 +0300

    Лекция 4, блок заданий 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+
+    //Ввести с клавиатуры два числа, и вывести на экран минимальное из них.
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        if (num1 > num2)
+            System.out.println(num2);
+         else System.out.println(num1);
+    }
 
 }
 
