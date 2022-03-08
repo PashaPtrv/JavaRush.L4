27eb594bee1d65262c26f71ad742d8af683b5163
Pasha <pvlptrv@yandex.ru>
2022-03-08 03:22:32 +0300

    Лекция 4, блок заданий 4.3

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    //Ввести с клавиатуры два числа m и n.
-    //Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.
+    //Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int m = scanner.nextInt();
-        int n = scanner.nextInt();
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(8);
             }
             System.out.println();
