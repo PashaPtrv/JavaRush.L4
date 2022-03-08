0466cefbaf94042b74ca22ec6dcf21fa055db869
Pasha <pvlptrv@yandex.ru>
2022-03-08 03:35:57 +0300

    Лекция 4, блок заданий 4.4

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,23 @@ import java.util.Scanner;
 
 public class Main {
 
-    //Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
+    //Используя цикл for вывести на экран:
+    //- горизонтальную линию из 10 восьмёрок
+    //- вертикальную линию из 10 восьмёрок.
 
     public static void main(String[] args) {
 
         for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(8);
+            System.out.print(8);
+        }
+        System.out.println();
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(8);
             }
-            System.out.println();
+
         }
     }
-}
+
 
 
 
